05659b5647c4d69492840e8dc6b4ec0986cf0421
Benjamin Foote <git@bnf.net>
2016-06-15 11:58:10 -0700

    improve fb token script

--- scripts/facebook-longer-token.sh
+++ scripts/facebook-longer-token.sh
@@ -3,18 +3,21 @@
 
 function usage() {
 cat <<EOF
-     Get a production token from Facebook that lasts 60 days
+    Get a production token from Facebook that lasts 60 days
 
-     usage:
-          $0 <fb_app_id> <fb_app_secret> <fb_user_token>
+    usage:
+         $0 <fb_app_id> <fb_app_secret> <fb_user_token>
 
-     the README has some instructions
+    the README has some instructions
 
-     get the app id and secret from
-     https://developers.facebook.com/apps/367708839993582/dashboard/
+    get the app id and secret from the app Dashboard at:
+    	Pender:			
+    		https://developers.facebook.com/apps/1610612792591538/dashboard/
+    	Pender-Develop:
+    		https://developers.facebook.com/apps/1637309523255198/dashboard/
 
-     get the user token from
-     https://developers.facebook.com/tools/accesstoken/
+    get the user token from
+    https://developers.facebook.com/tools/accesstoken/
 
 EOF
 exit;
@@ -30,5 +33,9 @@ fb_app_secret=$2
 
 fb_user_token=$3
 
-curl -s "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=${fb_app_id}&client_secret=${fb_app_secret}&fb_exchange_token=${fb_user_token}" | cut -d= -f2 | cut -d\& -f1
-# echo
+TOKEN=$(curl -s "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=${fb_app_id}&client_secret=${fb_app_secret}&fb_exchange_token=${fb_user_token}" | cut -d= -f2 | cut -d\& -f1)
+echo
+echo "fb returned:"
+echo
+echo $TOKEN
+echo
