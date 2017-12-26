4979ca2e52a5e3687e00f4c3759378daee062545
Caio Almeida <caiosba@gmail.com>
2017-12-26 18:45:27 -0300

    Ticket #6564: Fixing Code Climate

--- app/models/media.rb
+++ app/models/media.rb
@@ -46,6 +46,7 @@ class Media
   include ActiveModel::Conversion
   include MediasHelper
   include MediaScreenshot
+  include MediaOembed
   extend ActiveModel::Naming
 
   attr_accessor :url, :provider, :type, :data, :request, :doc, :original_url, :key
@@ -79,15 +80,6 @@ class Media
     include concern
   end
 
-  def self.as_oembed(data, original_url, maxwidth, maxheight, instance = nil)
-    return instance.send(:get_oembed_data, original_url, maxwidth, maxheight) if instance
-    if data[:raw].nil? || data[:raw][:oembed].nil?
-      Media.default_oembed(data, original_url, maxwidth, maxheight)
-    else
-      data[:raw][:oembed].merge(width: maxwidth, height: maxheight, html: Media.default_oembed_html(original_url, maxwidth, maxheight))
-    end
-  end
-
   def self.minimal_data(instance)
     data = {}
     %w(published_at username title description picture author_url author_picture author_name screenshot).each do |field|
@@ -118,29 +110,6 @@ class Media
       Rails.logger.warn "Could not access url: #{e.message}"
       return false
     end
-  end
-
-  def self.default_oembed(data, original_url, maxwidth = nil, maxheight= nil)
-    maxwidth ||= 800
-    maxheight ||= 200
-    src = original_url.gsub('medias.oembed', 'medias.html')
-    {
-      type: 'rich',
-      version: '1.0',
-      title: data['title'] || 'Pender',
-      author_name: data['username'],
-      author_url: (data['type'] === 'profile' ? data['url'] : ''),
-      provider_name: data['provider'],
-      provider_url: 'http://' + Media.parse_url(data['url']).host,
-      thumbnail_url: data['picture'],
-      html: Media.default_oembed_html(src, maxwidth, maxheight),
-      width: maxwidth,
-      height: maxheight
-    }.with_indifferent_access
-  end
-
-  def self.default_oembed_html(src, maxwidth = 800, maxheight = 200)
-    "<iframe src=\"#{src}\" width=\"#{maxwidth}\" height=\"#{maxheight}\" scrolling=\"no\" border=\"0\" seamless>Not supported</iframe>"
   end
 
   def self.get_id(url)
@@ -317,18 +286,6 @@ class Media
     rescue
       self.url.gsub!(/^https:/i, 'http:')
     end
-  end
-
-  def get_oembed_data(original_url = nil, maxwidth = nil, maxheight= nil)
-    url = original_url || self.url
-    if !self.data['raw'].nil? && !self.data['raw']['oembed'].nil?
-      self.data['raw']['oembed'].merge(width: maxwidth, height: maxheight, html: Media.default_oembed_html(url, maxwidth, maxheight))
-    else
-      self.as_json if self.data.empty?
-      %w(type provider).each { |key| self.data[key] = self.send(key.to_sym) }
-      self.data['raw']['oembed'] = Media.default_oembed(self.data, url, maxwidth, maxheight) unless self.data_from_oembed_item
-    end
-    self.data['raw']['oembed']
   end
 
   def redirect_https_to_http?(header_options, message)
