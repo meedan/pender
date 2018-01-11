176a222b26c7dfcb06d66ddd0e85ee5abe185cf2
Caio Almeida <caiosba@gmail.com>
2018-01-11 18:36:02 -0300

    Do not use the same settings as Sidekiq for Redis operations that are not related to Sidekiq

--- lib/semaphore.rb
+++ lib/semaphore.rb
@@ -1,20 +1,20 @@
 class Semaphore
   def initialize(key)
-    unless SIDEKIQ_CONFIG.nil?
+    unless CONFIG.nil?
       @key = "semaphore:#{key}"
-      @redis = Redis.new({ host: SIDEKIQ_CONFIG[:redis_host], port: SIDEKIQ_CONFIG[:redis_port], db: SIDEKIQ_CONFIG[:redis_database] })
+      @redis = Redis.new({ host: CONFIG[:redis_host], port: CONFIG[:redis_port], db: CONFIG[:redis_database] })
     end
   end
 
   def lock
-    @redis.getset(@key, Time.now) unless SIDEKIQ_CONFIG.nil?
+    @redis.getset(@key, Time.now) unless CONFIG.nil?
   end
 
   def locked?
-    SIDEKIQ_CONFIG.nil? ? false : !@redis.get(@key).nil?
+    CONFIG.nil? ? false : !@redis.get(@key).nil?
   end
 
   def unlock
-    @redis.del(@key) unless SIDEKIQ_CONFIG.nil?
+    @redis.del(@key) unless CONFIG.nil?
   end
 end
