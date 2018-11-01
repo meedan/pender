9347127733f9027df281263f659f5d5765aefcc9
infojunkie <karim.ratib@gmail.com>
2018-11-01 13:11:18 -0700

    Don't crash if cookies.txt is not here

--- config/initializers/load_cookies.rb
+++ config/initializers/load_cookies.rb
@@ -1,9 +1,11 @@
 COOKIES_PATH = 'config/cookies.txt'
 
 CONFIG['cookies'] ||= {}
-File.readlines(COOKIES_PATH).each do |line|
-  data = line.split("\t")
-  next if data[0].start_with?('#')
-  CONFIG['cookies'][data[0]] ||= {}
-  CONFIG['cookies'][data[0]][data[5]] = data[6].strip if data[6]
+if File.file?(COOKIES_PATH)
+  File.readlines(COOKIES_PATH).each do |line|
+    data = line.split("\t")
+    next if data[0].start_with?('#')
+    CONFIG['cookies'][data[0]] ||= {}
+    CONFIG['cookies'][data[0]][data[5]] = data[6].strip if data[6]
+  end
 end
