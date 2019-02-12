4083333b945fea69bb651d07b14226d4c3c92989
Daniela Soares Feitosa <daniela@meedan.com>
2019-02-12 13:26:10 -0300

    Ticket #7306: Notify Errbit and webhook when archival fails

--- app/models/concerns/media_archive_is_archiver.rb
+++ app/models/concerns/media_archive_is_archiver.rb
@@ -15,8 +15,8 @@ module MediaArchiveIsArchiver
       self.delay_for(15.seconds).send_to_archive_is(url, key_id)
     end
 
-    def send_to_archive_is(url, key_id, attempts = 1)
-      Media.give_up('archive_is', url, key_id, attempts) and return
+    def send_to_archive_is(url, key_id, attempts = 1, response = nil)
+      Media.give_up('archive_is', url, key_id, attempts, response) and return
 
       uri = URI.parse('http://archive.is/submit/')
       http = Net::HTTP.new(uri.host, uri.port)
@@ -26,11 +26,14 @@ module MediaArchiveIsArchiver
       Rails.logger.info "[Archiver Archive.is] Sending #{url} to Archive.is: Code: #{response.code} Response: #{response.body}"
 
       if response['refresh']
-        Media.delay_for(3.minutes).send_to_archive_is(url, key_id, attempts + 1)
+        Media.delay_for(3.minutes).send_to_archive_is(url, key_id, attempts + 1, {code: response.code, message: response.message})
       elsif response['location']
         data = { location: response['location'] }
         Media.notify_webhook_and_update_cache('archive_is', url, data, key_id)
       else
+        Airbrake.notify(StandardError.new('Unexpected response from archive.is'), parameters: {url: url, archiver: 'archive.is', error_code: response.code, error_message: response.message, error_body: response.body}) if Airbrake.configuration.api_key && !response.nil?
+        data = { error: { message: I18n.t(:could_not_archive, error_message: response.message), code: response.code }}
+        Media.notify_webhook_and_update_cache('archive_is', url, data, key_id)
         raise "Unexpected response from archive.is with code #{response.code}: #{response.body}"
       end
     end
