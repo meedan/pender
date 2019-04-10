3e18ce83bee0a1364458de9a6d1c89309e6b1d38
Mohamed <mhamzawey@aucegypt.edu>
2019-04-10 16:34:01 +0200

    removing chown and chmod in start.sh, add recursive chmod in directory creation inside Dockerfile.production

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -21,10 +21,10 @@ done
 cd ${DEPLOYDIR}/shared
 
 # these  are runtime volumes, linked to from ${DEPLOYDIR}/current/
-for DIR in cache db cookies screenshots; do
-  chown -R ${DEPLOYUSER}:www-data ${DEPLOYDIR}/shared/${DIR}
-  chmod -R 775 ${DEPLOYDIR}/shared/${DIR}
-done
+#for DIR in cache db cookies screenshots; do
+#  chown -R ${DEPLOYUSER}:www-data ${DEPLOYDIR}/shared/${DIR}
+#  chmod -R 775 ${DEPLOYDIR}/shared/${DIR}
+#done
 
 cd -
 
