21c35ef45df2b173daa7462849f9e7da8ee0b7c9
infojunkie <karim.ratib@gmail.com>
2019-04-14 11:22:59 -0700

    Update version manually [skip ci]

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,20 +1,20 @@
-VERSION = 'v0.70.0'
+VERSION = 'v0.76.0'
 
-if ENV['RAILS_ENV'] == 'development'
-  path = File.absolute_path(__FILE__)
-  begin
-    response = `curl https://api.github.com/repos/meedan/pender/tags`
-    parsed_response = JSON.parse response
-    if parsed_response.is_a?(Array) && parsed_response.first.dig('name')
-      latest_version = parsed_response.first.dig('name')
-      if latest_version != VERSION
-        VERSION.clear << latest_version
-        `sed -i "1s/VERSION = '.*'/VERSION = '#{VERSION}'/" #{path}`
-      end
-    elsif parsed_response.is_a?(Hash) && parsed_response.dig('message')
-      puts "Could not get latest version: `#{parsed_response.dig('message')}`"
-    end
-  rescue StandardError => e
-    puts "Could not get latest version: `#{e.inspect}`"
-  end
-end
+# if ENV['RAILS_ENV'] == 'development'
+#   path = File.absolute_path(__FILE__)
+#   begin
+#     response = `curl https://api.github.com/repos/meedan/pender/tags`
+#     parsed_response = JSON.parse response
+#     if parsed_response.is_a?(Array) && parsed_response.first.dig('name')
+#       latest_version = parsed_response.first.dig('name')
+#       if latest_version != VERSION
+#         VERSION.clear << latest_version
+#         `sed -i "1s/VERSION = '.*'/VERSION = '#{VERSION}'/" #{path}`
+#       end
+#     elsif parsed_response.is_a?(Hash) && parsed_response.dig('message')
+#       puts "Could not get latest version: `#{parsed_response.dig('message')}`"
+#     end
+#   rescue StandardError => e
+#     puts "Could not get latest version: `#{e.inspect}`"
+#   end
+# end
