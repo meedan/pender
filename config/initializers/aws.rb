f52cc41d03522344fb09d1853774021962c9cbb9
Daniela Feitosa <daniela@meedan.com>
2019-08-07 16:54:51 -0300

    Ticket #7591: Replacing aws-sdk gem by aws-sdk-s3 (#147)

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-require 'aws-sdk'
+require 'aws-sdk-s3'
 require 'pender_store'
 
 Aws.config.update(
--- lib/tasks/aws.rake
+++ lib/tasks/aws.rake
@@ -1,4 +1,4 @@
-require 'aws-sdk'
+require 'aws-sdk-s3'
 
 namespace :pender do
   namespace :aws do
