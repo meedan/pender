bfb0b9f204b5dc78731340e995a44549a3613d19
Daniela Feitosa <daniela@meedan.com>
2020-03-17 13:38:26 -0700

    Fix/6624 video archiving (#196)
    Ticket #6624: Video archiver fixes
    
    Fix Airbrake call
    Handle errors when youtube-dl call fails
    Avoid re-calling youtube-dl when retrying the archiving

--- app/models/concerns/media_video_archiver.rb
+++ app/models/concerns/media_video_archiver.rb
@@ -12,20 +12,23 @@ module MediaVideoArchiver
 
   module ClassMethods
     def archive_video_in_background(url, key_id)
-      self.delay_for(15.seconds).archive_video(url, key_id)
+      self.delay_for(15.seconds).send_to_video_archiver(url, key_id)
     end
 
-    def archive_video(url, key_id, attempts = 1, response = nil)
-      return if !supported_video?(url) || notify_video_already_archived(url, key_id)
-      id = Media.get_id(url)
-      local_folder = File.join(Rails.root, 'tmp', 'videos', id)
-      Media.give_up('video_archiver', url, key_id, attempts, response) and return
-      response = system("youtube-dl", url, "-o#{local_folder}/#{id}.%(ext)s", "--restrict-filenames", "--no-warnings", "-q", "--write-all-thumbnails", "--write-info-json", "--all-subs", "-fogg/mp4/webm",  out: '/dev/null')
+    def send_to_video_archiver(url, key_id, supported = nil, attempts = 1, response = nil)
+      handle_archiving_exceptions('video_archiver', 1.hour, url, key_id, attempts) do
+        supported = supported_video?(url) if supported.nil?
+        return if supported.is_a?(FalseClass) || notify_video_already_archived(url, key_id)
+        id = Media.get_id(url)
+        local_folder = File.join(Rails.root, 'tmp', 'videos', id)
+        Media.give_up('video_archiver', url, key_id, attempts, response) and return
+        response = system("youtube-dl", url, "-o#{local_folder}/#{id}.%(ext)s", "--restrict-filenames", "--no-warnings", "-q", "--write-all-thumbnails", "--write-info-json", "--all-subs", "-fogg/mp4/webm",  out: '/dev/null')
 
-      if response
-        ArchiveVideoWorker.perform_async(url, local_folder, self.archiving_folder, key_id)
-      else
-        Media.delay_for(3.minutes).archive_video(url, key_id, attempts + 1, {message: '[Youtube-DL] Cannot download video data', code: 5})
+        if response
+          ArchiveVideoWorker.perform_async(url, local_folder, self.archiving_folder, key_id)
+        else
+          Media.delay_for(3.minutes).send_to_video_archiver(url, key_id, supported, attempts + 1, {message: '[Youtube-DL] Cannot download video data', code: 5})
+        end
       end
     end
 
