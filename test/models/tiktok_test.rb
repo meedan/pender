cf9b966a93ade2656307d54d7736fa89d1a6bfab
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2020-06-26 19:27:46 -0300

    Feature/8185 review pender tests (#218)
    Ticket #8155: Remove tests with URL patterns tested in different places, merge and rename some, mock a test response, organize file and try to avoid flakes

--- test/models/tiktok_test.rb
+++ test/models/tiktok_test.rb
@@ -7,8 +7,8 @@ class TiktokTest < ActiveSupport::TestCase
     d = m.as_json
     assert_equal '@scout2015', d['username']
     assert_equal 'profile', d['type']
-    assert_equal 'Scout and Suki on TikTok', d['title']
-    assert_equal 'Scout and Suki on TikTok', d['author_name']
+    assert_match 'Scout and Suki', d['title']
+    assert_match 'Scout and Suki', d['author_name']
     assert_equal '@scout2015', d['external_id']
     assert_not_nil d['picture']
     assert_equal 'https://www.tiktok.com/@scout2015', m.url
@@ -23,7 +23,7 @@ class TiktokTest < ActiveSupport::TestCase
     assert_match /Who agrees/, d['title']
     assert_equal 'Scout and Suki', d['author_name']
     assert_equal '6771039287917038854', d['external_id']
-    assert_equal 'https://www.tiktok.com/@scout2015', d['author_url']
+    assert_match 'https://www.tiktok.com/@scout2015', d['author_url']
     assert_match /^http/, d['picture']
     assert_nil d['error']
   end
