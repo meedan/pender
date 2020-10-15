aee73b00e1f19f41e6c6db5db127fadb4a99ad1d
Daniel Shuy <daniel_shuy@hotmail.com>
2020-10-16 00:51:11 +0800

    Add Rswag tests for BaseApiController

--- test/controllers/base_api_controller_test.rb
+++ test/controllers/base_api_controller_test.rb
@@ -6,28 +6,6 @@ class BaseApiControllerTest < ActionController::TestCase
     @controller = Api::V1::BaseApiController.new
   end
 
-  test "should respond to json" do
-    assert_equal [:json], @controller.mimes_for_respond_to.keys
-  end
-
-  test "should filter parameters" do
-    authenticate_with_token
-    get :about, foo: 'bar', format: :json
-    assert_equal ['foo'], @controller.send(:get_params).keys
-  end
-
-  test "should return enabled archivers, name and version" do
-    authenticate_with_token
-    get :about, format: :json
-    assert_response :success
-    response = JSON.parse(@response.body)
-    assert_equal ['data', 'type'], response.keys.sort
-    assert_equal ['archivers', 'name', 'version'], response['data'].keys.sort
-    assert_equal 'about', response['type']
-    assert_equal 'Keep', response['data']['name']
-    assert_equal VERSION, response['data']['version']
-  end
-
   test "should return only enabled archivers" do
     authenticate_with_token
     get :about, format: :json
