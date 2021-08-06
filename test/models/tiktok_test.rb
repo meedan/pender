624e43493b1576b6c4c17a57f264cf79db3dd64f
Daniela Soares Feitosa <daniela@meedan.com>
2021-08-06 11:58:45 -0700

    Ticket CHECK-831: Fix tiktok test: less specific on author name

--- test/models/tiktok_test.rb
+++ test/models/tiktok_test.rb
@@ -22,7 +22,7 @@ class TiktokTest < ActiveSupport::TestCase
     assert_equal '@scout2015', data['username']
     assert_equal 'item', data['type']
     assert_match /Who agrees/, data['title']
-    assert_match 'Scout and Suki', data['author_name']
+    assert_match /Scout.+Suki/, data['author_name']
     assert_equal '6771039287917038854', data['external_id']
     assert_match 'https://www.tiktok.com/@scout2015', data['author_url']
     assert_match /^http/, data['picture']
