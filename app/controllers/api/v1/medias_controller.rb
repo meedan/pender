b1445e6dc4d772620466471be81d0ced9c1a2eba
Christa Hartsock <christa@meedan.com>
2022-08-22 16:13:47 -0700

    Convert TikTokProfile to parser class
    This class is the first time we need to get updated HTML from a parser
    class. As a result, we had to move the get_html function and make it
    usable in the context of our new classes. We can keep most of the
    previous state by passing an error-setting callback (since the existing
    parser modules and new parser classes both have different expectations
    of where errors will be set - and if we extract get_html to a separate
    class then there's no "self.data" to set info on as there was before it
    was split out of Media.
    
    This also pulls over the set_data_field function, since we need the
    ability to set specific fields only if they're not already set.

--- app/controllers/api/v1/medias_controller.rb
+++ app/controllers/api/v1/medias_controller.rb
@@ -25,7 +25,7 @@ module Api
           @id = Media.get_id(@url)
           (render_uncached_media and return) if @refresh || Pender::Store.current.read(@id, :json).nil?
           respond_to do |format|
-            list_formats.each do |f|
+            %w(html js json oembed).each do |f|
               format.send(f) { send("render_as_#{f}") }
             end
           end
---
+++ app/models/parser/tiktok_profile.rb
@@ -0,0 +1,47 @@
+module Parser
+  class TiktokProfile < Base
+    TIKTOK_PROFILE_URL = /^https?:\/\/(www\.)?tiktok\.com\/(?<username>[^\/\?]+)/
+
+    class << self
+      def type
+        'tiktok_profile'.freeze
+      end
+      
+      def patterns
+        [TIKTOK_PROFILE_URL]
+      end
+    end
+
+    def parse_data(doc)
+      match = url.match(TIKTOK_PROFILE_URL)
+      base_url = match[0] # Should this be set as canonical_url?
+      username = match['username']
+
+      set_data_field('external_id', username)
+      set_data_field('username', username)
+      set_data_field('title', username)
+      set_data_field('author_name', username)
+      set_data_field('description', base_url)
+
+      handle_exceptions(StandardError) do
+        doc = reparse_if_default_tiktok_page(doc, base_url) || doc
+        metatags = { picture: 'og:image', title: 'twitter:creator', description: 'description' }
+        @parsed_data.merge! get_html_metadata(doc, metatags)
+        set_data_field('author_name', parsed_data['title'], username)
+        @parsed_data.merge!({
+          author_name: parsed_data['title'],
+          author_picture: parsed_data['picture'],
+          author_url: base_url,
+          url: base_url
+        })
+      end
+      parsed_data
+    end
+
+    def reparse_if_default_tiktok_page(doc, base_url)
+      if doc.css('title').text == 'TikTok'
+        RequestHelper.get_html(base_url, set_error, RequestHelper.html_options(base_url), true)
+      end
+    end
+  end
+end
