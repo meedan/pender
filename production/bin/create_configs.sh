f50b596062d956f9200d30b1158490008fdf7dee
Martin Peck <martin@meedan.com>
2022-10-02 16:12:59 -0700

    Improve safety of configuration and exit if we are missing required config settings for QA or Live.

--- production/bin/create_configs.sh
+++ production/bin/create_configs.sh
@@ -23,10 +23,16 @@ echo "Starting application configuration. Processing ENV settings."
     cp cookies.txt.example config/cookies.txt
   fi
 
+  # If we are missing required configuration settings for QA and Live
+  # environments, emit an error message explaining the omissions.
+  #
+  missing_configs=""
+
   # Generate sidekiq config from SSM:
   WORKTMP=$(mktemp)
   if [[ -z ${sidekiq_config+x} ]]; then
     echo "Error: missing sidekiq_config ENV setting. Using defaults."
+    missing_configs="$missing_configs sidekiq_config,"
   else
     echo ${sidekiq_config} | base64 -d > $WORKTMP
     if (( $? != 0 )); then
@@ -43,6 +49,7 @@ echo "Starting application configuration. Processing ENV settings."
   WORKTMP=$(mktemp)
   if [[ -z ${database_config+x} ]]; then
     echo "Error: missing database_config ENV setting. Using defaults."
+    missing_configs="$missing_configs database_config,"
   else
     echo ${database_config} | base64 -d > $WORKTMP
     if (( $? != 0 )); then
@@ -59,6 +66,7 @@ echo "Starting application configuration. Processing ENV settings."
   WORKTMP=$(mktemp)
   if [[ -z ${environments_production+x} ]]; then
     echo "Error: missing environments_production ENV setting. Using defaults."
+    missing_configs="$missing_configs environments_production,"
   else
     echo ${environments_production} | base64 -d > $WORKTMP
     if (( $? != 0 )); then
@@ -70,6 +78,17 @@ echo "Starting application configuration. Processing ENV settings."
       sha1sum environments/production.rb
     fi
   fi
+
+  # As mentioned above, if we are missing required configs in QA or Live environemnts
+  # emit an error detailing the omitted configurations and exit with error.
+  # This will promptly fail an attempted deployment, rather than deferring to
+  # application restarts to indicate error.
+  if [[ "$DEPLOY_ENV" == "qa" || "$DEPLOY_ENV" == "live" ]]; then
+    if [[ "$missing_configs" != "" ]]; then
+      echo "Error: missing required configurations: $missing_configs exiting."
+      kill 0
+    fi
+  fi 
 )
 
 echo "Configuration complete."
