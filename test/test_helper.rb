dd588bfbf7bf9ad4feb8c7d8c386baad4e1fd01f
Christa Hartsock <christa@meedan.com>
2022-11-18 11:22:19 -0500

    Standardize test setup for perma cc

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -50,6 +50,7 @@ class ActiveSupport::TestCase
     Media.any_instance.stubs(:archive_to_archive_org).returns(nil)
     Media.any_instance.stubs(:archive_to_perma_cc).returns(nil)
     Media.any_instance.stubs(:archive_to_video).returns(nil)
+    Media::ARCHIVERS['perma_cc'][:enabled] = true
     Media::ARCHIVERS['archive_org'][:enabled] = true
     ApiKey.current = Pender::Store.current = PenderConfig.current = nil
     clear_bucket
@@ -62,6 +63,7 @@ class ActiveSupport::TestCase
   def teardown
     WebMock.reset!
     WebMock.allow_net_connect!
+    Media::ARCHIVERS['perma_cc'][:enabled] = false
     Media::ARCHIVERS['archive_org'][:enabled] = false
     clear_bucket
   end
