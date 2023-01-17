bb5c3f276dddb8e45e409576dee0bfcb8aa0b584
Christa Hartsock <christa@meedan.com>
2023-01-17 13:59:17 -0800

    Create TelegramItem parser and set title to URL
    Previously Telegram links were being parsed by the PageItem parser,
    which used the non-unique title as our title, which is used for similarity
    matching. Because Telegram sets the title of the page to the channel
    that published it, this meant that items were being marked as similar
    just because they were published by the same channel - not necessarily
    the same topic.
    
    To address this, this commit creates a TelegramItem parser that simplifies
    the fetching logic (mostly the same as what PageItem did), and sets the
    title as the URL since there is no other unique identifier provided (eg,
    no post title).
    
    Telegram profiles seemed to parse as I'd expect, so I continued to let
    those fall back to PageItem as they do now.
    
    CV2-2612

---
+++ test/models/parser/telegram_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class TelegramIntegrationTest < ActiveSupport::TestCase
+  test "should parse Telegram item URL and set unique title" do
+    m = create_media url: 'https://t.me/rechtsanwaeltin_beate_bahner/13285'
+    data = m.as_json
+    assert_equal 'rechtsanwaeltin_beate_bahner', data['username']
+    assert_equal 'item', data['type']
+    assert_equal 'telegram', data['provider']
+    assert_equal 'Rechtsanwältin Beate Bahner', data['author_name']
+    assert_equal 'https://t.me/rechtsanwaeltin_beate_bahner/13285', data['title']
+    assert_match /Pfizer bestätigt vor EU-Covid-Ausschuss/, data['description']
+    assert_nil data['error']
+  end
+end
+
+class TelegramProfileUnitTest < ActiveSupport::TestCase
+  def setup
+    isolated_setup
+  end
+
+  def teardown
+    isolated_teardown
+  end
+end
+
+class TelegramItemUnitTest <  ActiveSupport::TestCase
+  def setup
+    isolated_setup
+  end
+
+  def teardown
+    isolated_teardown
+  end
+
+  test "returns provider and type" do
+    assert_equal Parser::TelegramItem.type, 'telegram_item'
+  end
+
+  test "matches known kwai URL patterns, and returns instance on success" do
+    assert_nil Parser::TelegramItem.match?('https://example.com')
+
+    match_one = Parser::TelegramItem.match?('https://t.me/rechtsanwaeltin_beate_bahner/13285')
+    assert_equal true, match_one.is_a?(Parser::TelegramItem)
+
+    match_one = Parser::TelegramItem.match?('https://telegram.me/rechtsanwaeltin_beate_bahner/13285')
+    assert_equal true, match_one.is_a?(Parser::TelegramItem)
+  end
+
+  test "assigns values to hash from the HTML doc, and sets URL as title" do
+    doc = response_fixture_from_file('telegram-item.html', parse_as: :html)
+
+    data = Parser::TelegramItem.new('https://t.me/example_account/12345').parse_data(doc)
+    assert_equal 'https://t.me/example_account/12345', data[:title]
+    assert_match /❗ Pfizer bestätigt vor EU-Covid-Ausschuss/, data[:description]
+    assert_equal 'Rechtsanwältin Beate Bahner', data[:author_name]
+    assert_equal 'example_account', data[:username]
+    assert_equal '12345', data[:external_id]
+    assert_match /cdn4.telegram-cdn.org\/file/, data[:picture]
+  end
+end
