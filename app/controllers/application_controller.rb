97ba37df9eb518497cd9275563e39a62c59d9a30
manu vasconcelos <manu@meedan.com>
2023-05-12 11:23:29 -0300

    update protect_from_forgery
    We want to remove the CodeQL warning about CSRF.
    
    From the docs:
    APIs may want to disable this behavior since they are typically
    designed to be state-less: that is, the request API client handles the
    session instead of Rails. One way to achieve this is to use the
    :null_session strategy instead, which allows unverified requests to be
    handled, but with an empty session
    
    source:
    https://api.rubyonrails.org/classes/ActionController/RequestForgeryProtection.html#method-i-verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
 
   def process_action(*)
