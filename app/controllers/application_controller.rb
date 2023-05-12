79abd93c25cc037f1d50b47209dcfa69e8b62f19
manu vasconcelos <manu@meedan.com>
2023-05-12 17:11:27 -0300

    test removing protect_from_forgery and skip_before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token, unless: :csrf_required?
-
   def process_action(*)
     if PenderConfig.get('memory_report', false).to_s == 'true'
      MemoryProfiler.report { super }.pretty_print(detailed_report: true, scale_bytes: true)
     else
       super
     end
-  end
-
-  private
-
-  def csrf_required?
-    header = PenderConfig.get('authorization_header') || 'X-Token'
-    request.headers[header].blank?
   end
 end
