7a46582f283280ece9af4bfc08d85476f9636283
manu vasconcelos <manu@meedan.com>
2023-05-12 17:01:24 -0300

    add condition to ignore verification if an API key is present
    If we have an API key we don't need to verify the session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, unless: :csrf_required?
 
   def process_action(*)
     if PenderConfig.get('memory_report', false).to_s == 'true'
@@ -8,5 +8,12 @@ class ApplicationController < ActionController::Base
     else
       super
     end
+  end
+
+  private
+
+  def csrf_required?
+    header = PenderConfig.get('authorization_header') || 'X-Token'
+    request.headers[header].blank?
   end
 end
