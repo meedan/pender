9f75517b27cb0ef1e92e54ad20fa092aa2c25f3e
manu vasconcelos <manu@meedan.com>
2023-07-28 09:29:48 -0300

    make the query easier to read and update

--- app/models/twitter_client.rb
+++ app/models/twitter_client.rb
@@ -2,11 +2,19 @@ require 'json'
 require 'net/http'
 
 class TwitterClient
-  # tweet_ids = "1684310862842982400"
+  # tweet_id = "1684310862842982400"
   BASE_URI = "https://api.twitter.com/2/"
 
   def self.tweet_lookup(tweet_id)
-    get "tweets?ids=#{tweet_id}&tweet.fields=author_id,created_at,text&expansions=author_id,attachments.media_keys&user.fields=profile_image_url,username,url&media.fields=url"
+    params = {
+      "ids": tweet_id,
+      "tweet.fields": "author_id,created_at,text",
+      "expansions": "author_id,attachments.media_keys",
+      "user.fields": "profile_image_url,username,url",
+      "media.fields": "url"
+    }
+
+    get "tweets?#{Rack::Utils.build_nested_query(params)}"
   end
 
   def user_lookup_by_username(username)
@@ -21,7 +29,6 @@ class TwitterClient
     http.use_ssl = true
 
     headers = {
-      # "User-Agent": "v2TweetLookupRuby",
       "Authorization": "Bearer #{PenderConfig.get('twitter_bearer_token')}",
       "Content-Type": "application/json"
     }
@@ -32,35 +39,3 @@ class TwitterClient
     JSON.parse(response.body) 
     end
 end
-
-
-
-# headers = { 'User-Agent' => 'Mozilla/5.0 (compatible; Pender/0.1; +https://github.com/meedan/pender)' }.merge(RequestHelper.get_cf_credentials(uri))
-# request = Net::HTTP::Get.new(uri.request_uri, headers)
-# response = http.request(request)
-
-# from twitter github
-# params = {
-# 	"ids": tweet_ids,
-#   # "expansions": "author_id,referenced_tweets.id",
-#   "tweet.fields": "attachments,author_id,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang",
-#   # "user.fields": "name"
-#   # "media.fields": "url", 
-#   # "place.fields": "country_code",
-#   # "poll.fields": "options"
-# }
-
-# def tweet_lookup(url, bearer_token, params)
-#   options = {
-#     method: 'get',
-#     headers: {
-#       "User-Agent": "v2TweetLookupRuby",
-#       "Authorization": "Bearer #{bearer_token}"
-#     },
-#     params: params
-#   }
-
-#   request = Typhoeus::Request.new(url, options)
-#   response = request.run
-
-#   return response
