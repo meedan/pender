38cfe4414ae3cd115ca5df92b79d8fb50e899f55
manu vasconcelos <manu@meedan.com>
2023-09-05 15:38:59 -0300

    fix regex to be less broad
    trying to fit everything into two patterns didn't work, it ended up
    matching the twitter item as well. So I broke it into two more patterns,
    and ordered them from the two that are more specific to the less.

--- app/models/parser/twitter_profile.rb
+++ app/models/parser/twitter_profile.rb
@@ -9,8 +9,10 @@ module Parser
 
       def patterns
         [
-          /^https?:\/\/(www\.)?twitter\.com\/(?<username>[\w\d]+)(\??.*)$/,
-          /^https?:\/\/(0|m|mobile)\.twitter\.com\/(?<username>[\w\d]+)(\??.*)$/
+          /^https?:\/\/(www\.)?twitter\.com\/(?<username>[\w\d]+)(\?+.*)$/,
+          /^https?:\/\/(0|m|mobile)\.twitter\.com\/(?<username>[\w\d]+)(\?+.*)$/,
+          /^https?:\/\/(www\.)?twitter\.com\/(?<username>[^\/]+)$/,
+          /^https?:\/\/(0|m|mobile)\.twitter\.com\/(?<username>[^\/]+)$/
         ]
       end
     end
