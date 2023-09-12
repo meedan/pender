40539947c3efc1ef070794a72ef6284248647642
manu vasconcelos <manu@meedan.com>
2023-09-12 18:02:47 -0300

    add failed attempt to integration tests

--- test/integration/parsers/facebook_item_test.rb
+++ test/integration/parsers/facebook_item_test.rb
@@ -19,5 +19,25 @@ class FacebookItemIntegrationTest < ActiveSupport::TestCase
     assert !data['picture'].blank?
     assert !data['subtype'].blank?
     assert !data['published_at'].blank?
+    assert !data['html'].blank?
+  end
+
+  test "should return data even if post does not exist" do
+    m = create_media url: 'https://www.facebook.com/111111111111111/posts/1111111111111111'
+    data = m.as_json
+
+    assert_equal 'facebook', data['provider']
+    assert_equal 'item', data['type']
+    assert_equal '111111111111111_1111111111111111', data['external_id']
+    assert_equal 'https://www.facebook.com/111111111111111/posts/1111111111111111', data['title']
+    assert !data['raw']['crowdtangle']['error'].blank?
+    assert_equal '', data['username']
+    assert_equal '', data['author_name']
+    assert_equal '', data['author_picture']
+    assert_equal '', data['author_url']
+    assert_equal '', data['description']
+    assert_equal '', data['picture']
+    assert_equal '', data['published_at']
+    assert !data['html'].blank?
   end
 end
