67ea8e881d04f50f78193dd4b471ec1b3c1466d1
manu vasconcelos <manu@meedan.com>
2023-09-18 17:30:20 -0300

    add failed attempt to integration tests

--- test/integration/parsers/facebook_profile_test.rb
+++ test/integration/parsers/facebook_profile_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class FacebookProfileIntegrationTest < ActiveSupport::TestCase
   test "should parse Facebook page" do
-    m = create_media url: 'https://www.facebook.com/ironmaiden/?fref=ts'
-    data = m.as_json
+    media = create_media url: 'https://www.facebook.com/ironmaiden/?fref=ts'
+    data = media.as_json
     
     assert !data['title'].blank?
-    assert_match 'ironmaiden', data['username']
+    assert_equal 'ironmaiden', data['username']
     assert_equal 'facebook', data['provider']
     assert_equal 'profile', data['type']
 
@@ -16,20 +16,33 @@ class FacebookProfileIntegrationTest < ActiveSupport::TestCase
   end
 
   test "should parse Facebook page with numeric id" do
-    m = create_media url: 'https://www.facebook.com/pages/Meedan/105510962816034?fref=ts'
-    data = m.as_json
+    media = create_media url: 'https://www.facebook.com/pages/Meedan/105510962816034?fref=ts'
+    data = media.as_json
     
     assert !data['title'].blank?
-    assert_match 'Meedan', data['username']
-    assert_equal 'facebook', data['provider']
-    assert_equal 'profile', data['type']
+    assert_equal 'Meedan', data['username']
     assert_not_nil data['description']
     assert_not_nil data['picture']
     assert_not_nil data['published_at']
+    assert_equal 'facebook', data['provider']
+    assert_equal 'profile', data['type']
 
     # Parsed from URL
     assert_equal '105510962816034', data['id']
     assert_equal '105510962816034', data['external_id']
+  end
+
+  test "should return data even if Facebook page does not exist" do
+    media = create_media url: 'https://www.facebook.com/pages/fakepage/1111111111111'
+    data = media.as_json
+
+    assert_equal 'https://www.facebook.com/pages/fakepage/1111111111111', data['title']
+    assert_equal 'fakepage', data['username']
+    assert data['description'].blank?
+    assert data['picture'].blank?
+    assert data['published_at'].blank?
+    assert_equal 'facebook', data['provider']
+    assert_equal 'profile', data['type']
   end
 end
 
