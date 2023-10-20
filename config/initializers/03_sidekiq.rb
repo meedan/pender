403e99b7e5d6e76fb07ee3b2dc3015427cdd27c8
manu vasconcelos <manu@meedan.com>
2023-10-20 10:34:08 -0300

    update death handler to notify when it's not a RetryLimitHit
    Some errors where happening during a delayed job (`get_archive_org_status`),
    so our `handle_archiving_exceptions` wasn't handling those.
    
    Meaning they would be retried multiple times without being re-raised
    as RetryLater. When they hit the death handler limit_hit_exception would
    be nil because they would fail
    `if original_exception.is_a?(Pender::Exception::RetryLater)`
    
    We are now handling errors inside `get_archive_org_status`, but we are
    adding the else here as an extra safety measure. We want to see what kind
    of errors we get for a while and then decide if this i needed or not, and
    what the best way to handle this is.

--- config/initializers/03_sidekiq.rb
+++ config/initializers/03_sidekiq.rb
@@ -13,8 +13,10 @@ if File.exist?(file)
     config.death_handlers << ->(job, original_exception) do
       if original_exception.is_a?(Pender::Exception::RetryLater)
         limit_hit_exception = Pender::Exception::RetryLimitHit.new(original_exception)
+        PenderSentry.notify(limit_hit_exception, { job: job, original_exception: original_exception.cause.inspect })
+      else
+        PenderSentry.notify(original_exception, { job: job })
       end
-      PenderSentry.notify(limit_hit_exception, {job: job, original_exception: original_exception.cause.inspect})
     end
   end
 
