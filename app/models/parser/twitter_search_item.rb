e167891b0fee0ac429caa9cc138cfe4c2db9d945
Jay Joshua <7008757+jayjay-w@users.noreply.github.com>
2023-12-07 14:52:43 +0100

    Add reviewer feedback
    Add reviewer feedback:
     - Escape URLs sent to parser
     - Handle different domains (m.twitter.com and mobile.twitter.com)
     - Move src to api

--- app/models/parser/twitter_search_item.rb
+++ app/models/parser/twitter_search_item.rb
@@ -2,7 +2,7 @@ module Parser
   class TwitterSearchItem < Base
     include ProviderTwitter
 
-    TWITTER_SEARCH_ITEM_URL = /https:\/\/twitter\.com\/search[^\s"]*/
+    TWITTER_SEARCH_ITEM_URL = /^https?:\/\/((0|m|mobile|www)\.)?twitter\.com\/search\?(?:[^&\s"]+&)*q=[^&\s"]+/
 
     class << self
       def type
@@ -19,7 +19,8 @@ module Parser
     # Main function for class
     def parse_data_for_parser(_doc, _original_url, _jsonld_array)
       handle_exceptions(StandardError) do
-        params = URI.parse(url).query.split('&').map { |pair| pair.split('=') }.to_h
+        uri = RequestHelper.parse_url(url)
+        params = uri.query.split('&').map { |pair| pair.split('=') }.to_h
         src = params['src']
         search_term = params['q']
 
@@ -27,7 +28,10 @@ module Parser
           title: search_term,
           search_term: search_term,
           src: src,
-          description: "Twitter search for #{search_term}"
+          description: uri,
+          raw: {
+            src: src
+          }
         )
       end
       parsed_data
