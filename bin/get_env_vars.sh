88dae53f68b6733969bb7dc4a48c888fab7ffd79
manu vasconcelos <manu@meedan.com>
2023-12-15 14:23:34 -0300

    change .txt to .env

--- bin/get_env_vars.sh
+++ bin/get_env_vars.sh
@@ -4,13 +4,16 @@ aws sts get-caller-identity >/dev/null 2>&1
 if (( $? != 0 )); then
   echo "Error calling AWS get-caller-identity. Do you have valid credentials?"
 else 
-  SSM_NAMES=$(aws ssm get-parameters-by-path --region $REGION --path /local/pender/ --recursive --with-decryption --output text --query "Parameters[].[Name]")
+  SSM_NAMES=$(aws ssm get-parameters-by-path --region $REGION --path /test/pender/ --recursive --with-decryption --output text --query "Parameters[].[Name]")
   echo "Getting variables"
   for NAME in $SSM_NAMES; do
     echo "."
     VALUE=$(aws ssm get-parameters --region $REGION --with-decryption --name "$NAME" | jq .Parameters[].Value)
     VARNAME=$(basename "$NAME")
 
-    echo "$VARNAME: $VALUE" >> config/config-test.yml
+    echo "$VARNAME: $VALUE" >> web-variables.env
   done
+
+  cat web-variables.env
+  rm web-variables.env
 fi
