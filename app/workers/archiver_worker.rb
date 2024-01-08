b43a1f1c40bc23b746ec5e5e68771ae38c756766
manu vasconcelos <manu@meedan.com>
2024-01-08 17:08:16 -0300

    Update retry schedule for archiver_worker
    For this one we have to remember that this is the same worker for perma_cc
    as for archive_org. We need to delay when archive_org retries and make longer
    intervals for each retry, but that might not be necessary for perma.
    
    What changed:
    1. Starting later: Instead of adding 15 s we add 300, this means it retries
    for the first time after five minutes. We want to retry after five minutes
    because that is how long our IP is blocked if we hit the rate limit.
    2. Increasing intervals between retries: Adding 300s instead of 15 helps
    with that, but I also doing count ** 4.5 instead of ** 4 to help with that.
    3. Retrying for 24 hours: for both archivers I think 24 hours should be
    the maximum we try for. After that I think it becomes increasingly possible
    that the url is no longer the version the user might have wanted to store
    
    Notes:
    1. I still need to test this
    2. They might return a header with the information on when to retry, I'll
    look into that
    
    What the time looks like (considering sidekiq 25 retrials)
    
    time: 5 minutes 5 seconds
    time: 5 minutes 28 seconds
    time: 7 minutes 28 seconds
    time: 14 minutes 7 seconds
    time: 28 minutes 35 seconds
    time: 58 minutes 43 seconds
    time: 1 hours 51 minutes 48 seconds
    time: 3 hours 18 minutes 59 seconds
    time: 5 hours 34 minutes 33 seconds
    time: 8 hours 52 minutes 57 seconds
    time: 13 hours 35 minutes 54 seconds
    time: 20 hours 4 minutes 8 seconds
    time: 1 days 4 hours 43 minutes 10 seconds
    time: 1 days 16 hours 2 minutes 9 seconds
    time: 2 days 6 hours 33 minutes 5 seconds
    time: 3 days 55 minutes 29 seconds
    time: 3 days 23 hours 45 minutes 55 seconds
    time: 5 days 3 hours 47 minutes 55 seconds
    time: 6 days 13 hours 52 minutes 36 seconds
    time: 8 days 6 hours 50 minutes 41 seconds
    time: 10 days 7 hours 42 minutes 1 seconds
    time: 12 days 17 hours 19 minutes 33 seconds
    time: 15 days 12 hours 54 minutes 50 seconds
    time: 18 days 19 hours 37 minutes 18 seconds
    time: 22 days 14 hours 40 minutes 7 seconds

--- app/workers/archiver_worker.rb
+++ app/workers/archiver_worker.rb
@@ -1,7 +1,13 @@
 class ArchiverWorker
   include Sidekiq::Worker
+  sidekiq_options retry_for: 24.hours
 
   sidekiq_retries_exhausted { |msg, e| retries_exhausted_callback(msg, e) }
+
+  sidekiq_retry_in do |count, exception, jobhash|
+    return unless exception.is_a? Pender::Exception::ArchiveOrgError
+    (count ** 4.5) + 300 + (rand(10) * (count + 1))
+  end
 
   def self.retries_exhausted_callback(msg, _e)
     Media.give_up(msg.with_indifferent_access)
