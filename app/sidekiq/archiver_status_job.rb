134a2b7aa7b8f437d56dcdc34d344728be084311
manu vasconcelos <manu@meedan.com>
2024-01-08 16:27:37 -0300

    update retry schedule for ArchiverStatusJob
    The changes here are:
    
    1. adding 60 seconds instead of 15 → it’s a small change but helps us
    start a bit later, a makes the intervals a bit longer, I think this is
    enough to help, as this happens when we are waiting for the request to
    be processed by archive_org
    2. trying for 24 hours → I’m not sure about this one, I think it makes
    more sense for the archiving job, but if we haven’t been able to get the
    status after 24 hours should we keep trying? It seems unnecessary, wasteful?,
    to keep trying for more than that.
    
    what the time looks like (considering sidekiq 25 retrials)
      #1
      time: 1 minutes 11 seconds
      #2
      time: 1 minutes 19 seconds
      #3
      time: 2 minutes 41 seconds
      #4
      time: 5 minutes 36 seconds
      #5
      time: 12 minutes 19 seconds
      #6
      time: 23 minutes 32 seconds
      #7
      time: 41 minutes 25 seconds
      #8
      time: 1 hours 9 minutes 34 seconds
      #9
      time: 1 hours 51 minutes 1 seconds
      #10
      time: 2 hours 49 minutes 19 seconds
      #11
      time: 4 hours 6 minutes 13 seconds
      #12
      time: 5 hours 47 minutes 2 seconds
      #13
      time: 7 hours 57 minutes 1 seconds
      #14
      time: 10 hours 42 minutes 16 seconds
      #15
      time: 14 hours 6 minutes 5 seconds
      #16
      time: 18 hours 13 minutes 16 seconds
      #17
      time: 23 hours 13 minutes 37 seconds
      #18
      time: 1 days 5 hours 11 minutes 33 seconds
      #19
      time: 1 days 12 hours 14 minutes 41 seconds
      #20
      time: 1 days 20 hours 29 minutes 25 seconds
      #21
      time: 2 days 6 hours 2 minutes 21 seconds
      #22
      time: 2 days 17 hours 6 minutes 25 seconds
      #23
      time: 3 days 5 hours 47 minutes 49 seconds
      #24
      time: 3 days 20 hours 11 minutes 26 seconds
      #25
      time: 4 days 12 hours 33 minutes 9 seconds

--- app/sidekiq/archiver_status_job.rb
+++ app/sidekiq/archiver_status_job.rb
@@ -1,5 +1,10 @@
 class ArchiverStatusJob
   include Sidekiq::Job
+  sidekiq_options retry_for: 24.hours
+
+  sidekiq_retry_in do |count|
+    (count ** 4) + 60 + (rand(10) * (count + 1))
+  end
 
   def perform(job_id, url, key_id)
     Media.get_archive_org_status(job_id, url, key_id)
