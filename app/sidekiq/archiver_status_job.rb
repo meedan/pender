7273cac923f76a19a6b77c946120cc7336e2b455
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2024-01-10 17:23:01 -0300

    4076 – Adjust archive_org job retry schedule (#427)
    archiver_status_job:
    
    - start retrying: aprox. after a minute, that should be more than enough (it usually doesn't take that long)
    - interval: just add a bit of more time, instead of 15, 60. It’s a small change but helps us start a bit later, a makes the intervals a bit longer, I think this is enough to help, as this happens when we are waiting for the request to be processed by archive_org
    - how long: 24 hours. I’m not sure about this one, I think it makes more sense for the archiving job, but if we haven’t been able to get the status after 24 hours should we keep trying? It seems unnecessary, wasteful?, to keep trying for more than that.
    
    archiver_worker:
    
    - start retrying: after 5 minutes
    - interval: I think ramping up to 4.5 and starting and always adding 5 minutes might be enough. It starts later and increases the intervals faster
    - for how long: 24 hours. I think this should be the maximum we try for. After that I think it becomes increasingly possible that the url is no longer the version the user might have wanted to store
    - the schedule of perma cc and archive org will be the same
        - the exception is always RetryLater, so we are not able to differentiate
       - I'm prioritizing archive org schedule because:
          - it’s the one we use the most
          - it’s the one with the most problems
    
    Obs.: For both I used sidekiqs own retrying/backing off formula as a starting point.(here)[https://github.com/sidekiq/sidekiq/wiki/Error-Handling#automatic-job-retry]

--- app/sidekiq/archiver_status_job.rb
+++ app/sidekiq/archiver_status_job.rb
@@ -1,5 +1,23 @@
-class ArchiverStatusJob
+# Notes on the rationale for retrying
+# 1. I used sidekiqs own retrying/backing off formula as a starting point
+#  https://github.com/sidekiq/sidekiq/wiki/Error-Handling#automatic-job-retry
+# 2. start retrying:
+  # aprox. after a minute, that should be more than enough (it usually doesn't take that long)
+# 3. interval:
+  # just add a bit of more time, instead of 15, 60.
+  # It’s a small change but helps us start a bit later, a makes the intervals a bit longer.
+  # I think this is enough to help, as this happens when we are waiting for the request to be processed by archive_org
+# 4. for how long:
+  # 24 hours.
+  # If we haven’t been able to get the status after 24 hours should it seems wasteful to keep trying
+
+  class ArchiverStatusJob
   include Sidekiq::Job
+  sidekiq_options retry_for: 24.hours
+
+  sidekiq_retry_in do |count|
+    (count ** 4) + 60 + (rand(10) * (count + 1))
+  end
 
   def perform(job_id, url, key_id)
     Media.get_archive_org_status(job_id, url, key_id)
--- app/workers/archiver_worker.rb
+++ app/workers/archiver_worker.rb
@@ -1,7 +1,29 @@
+# Notes on the rationale for retrying
+# 1. I used sidekiqs own retrying/backing off formula as a starting point
+#  https://github.com/sidekiq/sidekiq/wiki/Error-Handling#automatic-job-retry
+# 2. start retrying:
+  # after 5 minutes, because of archive.org: https://archive.org/details/toomanyrequests_20191110
+# 3. interval:
+  # I think ramping up to 4.5 and starting and always adding 5 minutes might be enough.
+  # It starts later and increases the intervals faster
+# 4. for how long:
+  # 24 hours.
+  # After that I think it becomes increasingly possible that the url is no longer the version the user might have wanted to store
+# 5. this schedule is for both archive.org and perma.cc
+  # because we are unable to specify by the exception (it's always RetryLater)
+  # We prioritized archive org schedule because:
+  #  - it’s the one we use the most
+  #  - it’s the one with the most problems
+
 class ArchiverWorker
   include Sidekiq::Worker
+  sidekiq_options retry_for: 24.hours
 
   sidekiq_retries_exhausted { |msg, e| retries_exhausted_callback(msg, e) }
+
+  sidekiq_retry_in do |count|
+    (count ** 4.5) + 300 + (rand(10) * (count + 1))
+  end
 
   def self.retries_exhausted_callback(msg, _e)
     Media.give_up(msg.with_indifferent_access)
