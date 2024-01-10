da54f1adc4333daf0ca096e082a3f0b73d587447
manu vasconcelos <manu@meedan.com>
2024-01-10 17:11:16 -0300

    add comment and explain the rationale behind the decisions

--- app/workers/archiver_worker.rb
+++ app/workers/archiver_worker.rb
@@ -1,3 +1,20 @@
+# Notes on the rationale for retrying
+# 1. I used sidekiqs own retrying/backing off formula as a starting point
+#  https://github.com/sidekiq/sidekiq/wiki/Error-Handling#automatic-job-retry
+# 2. start retrying:
+  # after 5 minutes, because of archive.org: https://archive.org/details/toomanyrequests_20191110
+# 3. interval:
+  # I think ramping up to 4.5 and starting and always adding 5 minutes might be enough.
+  # It starts later and increases the intervals faster
+# 4. for how long:
+  # 24 hours.
+  # After that I think it becomes increasingly possible that the url is no longer the version the user might have wanted to store
+# 5. this schedule is for both archive.org and perma.cc
+  # because we are unable to specify by the exception (it's always RetryLater)
+  # We prioritized archive org schedule because:
+  #  - it’s the one we use the most
+  #  - it’s the one with the most problems
+
 class ArchiverWorker
   include Sidekiq::Worker
   sidekiq_options retry_for: 24.hours
