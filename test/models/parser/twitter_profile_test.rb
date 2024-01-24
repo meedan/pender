b3ce7a339c674df8457e331fe44021f78de2caf1
manu vasconcelos <manu@meedan.com>
2024-01-24 10:52:39 -0300

    update twitter profile tests that failed when running action

--- test/models/parser/twitter_profile_test.rb
+++ test/models/parser/twitter_profile_test.rb
@@ -93,7 +93,12 @@ class TwitterProfileUnitTest < ActiveSupport::TestCase
     
     WebMock.stub_request(:get, "https://api.twitter.com/2/users/by")
       .with(query: query)
-      .with(headers: { "Authorization": "Bearer test" })
+      .with(headers: { 
+        "Authorization": "Bearer test", 
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Ruby'
+      })
       .to_return(status: 200, body: response_fixture_from_file('twitter-profile-response-success.json'))
 
     data = Parser::TwitterProfile.new('https://m.twitter.com/fake_user').parse_data(empty_doc)
@@ -107,7 +112,12 @@ class TwitterProfileUnitTest < ActiveSupport::TestCase
 
     WebMock.stub_request(:get, "https://api.twitter.com/2/users/by")
       .with(query: query)
-      .with(headers: { "Authorization": "Bearer test" })
+      .with(headers: { 
+        "Authorization": "Bearer test", 
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Ruby'
+      })
       .to_return(status: 404, body: response_fixture_from_file('twitter-profile-response-error.json'))
 
     sentry_call_count = 0
@@ -129,7 +139,12 @@ class TwitterProfileUnitTest < ActiveSupport::TestCase
 
     WebMock.stub_request(:get, "https://api.twitter.com/2/users/by")
       .with(query: query)
-      .with(headers: { "Authorization": "Bearer test" })
+      .with(headers: { 
+        "Authorization": "Bearer test", 
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Ruby'
+      })
       .to_raise(Errno::EHOSTUNREACH)
 
     sentry_call_count = 0
