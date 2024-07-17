aa5bb30e86f4757c553e9a2b3b992ed6295e75ba
manu vasconcelos <manu@meedan.com>
2024-07-17 16:21:15 -0300

    fix metrics_service_test

--- test/lib/metrics_service_test.rb
+++ test/lib/metrics_service_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class MetricsServiceTest < ActiveSupport::TestCase
   test "custom_counter works for real" do
     assert_nothing_raised do
-      MetricsService.custom_counter(:custom_counter, 'Custom counter test', labels: [:test])
+      MetricsService.custom_counter(:custom_counter, 'Custom counter test', labels: [:service_name, :test])
     end
   end
 
   test "increment_counter works for real" do
-    MetricsService.custom_counter(:custom_counter_2, 'Custom counter test', labels: [:test])
+    MetricsService.custom_counter(:custom_counter_2, 'Custom counter test', labels: [:service_name, :test])
 
     assert_nothing_raised do
       MetricsService.increment_counter(:custom_counter_2, labels: [:test])
@@ -16,7 +16,7 @@ class MetricsServiceTest < ActiveSupport::TestCase
   end
 
   test "get_counter works for real" do
-    custom_counter = MetricsService.custom_counter(:custom_counter_3, 'Custom counter test', labels: [:test])
+    custom_counter = MetricsService.custom_counter(:custom_counter_3, 'Custom counter test', labels: [:service_name, :test])
 
     assert_nothing_raised do
       MetricsService.get_counter(custom_counter, labels: [:test])
