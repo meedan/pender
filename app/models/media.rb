3e7cb3686ac68817f9f49654cd7d5f1403cf89ef
manu vasconcelos <manu@meedan.com>
2024-07-17 16:50:31 -0300

    make sure we are able to get the host
    We had one error where URI wasn't able to deal with non-ASCII
    characters. In order to fix that I added the normalize_url.

--- app/models/media.rb
+++ app/models/media.rb
@@ -328,9 +328,11 @@ class Media
   end
 
   def parser_requests_metrics
+    url = RequestHelper.normalize_url(self.url)
+
     MetricsService.increment_counter(:parser_requests_total)
     MetricsService.increment_counter(:parser_requests_per_parser, labels: { parser_name: data[:provider], parsing_status: data[:error].nil? ?  'success' : 'error' })
-    MetricsService.increment_counter(:parser_requests, labels: { parser_name: data[:provider], parsed_host: URI(data[:url]).host, parsing_status: data[:error].nil? ?  'success' : 'error' })
+    MetricsService.increment_counter(:parser_requests, labels: { parser_name: data[:provider], parsed_host: URI(url).host, parsing_status: data[:error].nil? ?  'success' : 'error' })
     if data[:error].nil?
       MetricsService.increment_counter(:parser_requests_success)
       MetricsService.increment_counter(:parser_requests_success_per_parser, labels: { parser_name: data[:provider] })
