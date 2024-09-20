ca050d28b24aa483868a246cb75215498aad2948
Doci Mou <22457019+dmou@users.noreply.github.com>
2024-09-20 15:27:42 -0400

    comment out path mods

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   config.lograge.enabled = true
 
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  # config.logger = ActiveSupport::Logger.new(STDOUT)
   config.lograge.logger = ActiveSupport::Logger.new(STDOUT)
   config.lograge.custom_options = lambda do |event|
     options = event.payload.slice(:request_id, :user_id)
@@ -58,5 +58,5 @@ Rails.application.configure do
   config.log_level = :warn
   config.paths['log'] = "/var/log/#{ENV['DEPLOY_ENV']}.log"
   config.paths['tmp'] = "/tmp"
-  config.paths['db'] = "/opt"
+  # config.paths['db'] = "/opt"
 end
