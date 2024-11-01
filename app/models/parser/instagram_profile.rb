9eba936ce2b4121c4546b995116b704e2c10594c
Jay Joshua <joejoshw@gmail.com>
2024-11-01 13:24:00 +0100

    Improve parsing of instagram profiles

--- app/models/parser/instagram_profile.rb
+++ app/models/parser/instagram_profile.rb
@@ -26,29 +26,42 @@ module Parser
       @parsed_data.merge!(
         'external_id' => username,
         'username' => "@#{username}",
-        'title' => username,
-        'description' => url
+        'title' => username
       )
 
       handle_exceptions(StandardError) do
         apify_data = get_instagram_profile_data_from_apify(username)
         return unless apify_data
 
-        @parsed_data['raw']['apify'] = apify_data
+        @parsed_data['raw']['apify'] = apify_data[0]
 
         # Update fields with Apify data
-        @parsed_data['description'] = parsed_data.dig('raw', 'apify', 'biography')
-        set_data_field('picture', parsed_data.dig('raw', 'apify', 'profile_pic_url'))
-        set_data_field('author_name', parsed_data.dig('raw', 'apify', 'full_name'))
-        set_data_field('author_picture', parsed_data.dig('raw', 'apify', 'profile_pic_url'))
+        set_data_field('picture', parsed_data.dig('raw', 'apify', 'profilePicUrl'))
+        set_data_field('author_url', url)
+        set_data_field('username', parsed_data.dig('raw', 'apify', 'fullName'))
+        set_data_field('description', parsed_data.dig('raw', 'apify', 'biography'))
+        set_data_field('author_name', parsed_data.dig('raw', 'apify', 'fullName'))
+        set_data_field('author_picture', parsed_data.dig('raw', 'apify', 'profilePicUrl'))
+        set_data_field('picture', @parsed_data['raw']['apify']['profilePicUrl'])
         set_data_field('published_at', '')
       end
+
+      @parsed_data['description'] ||= url
+      @parsed_data['html'] = html_for_instagram_profile(doc, url) || ''
       parsed_data
     end
 
     def get_instagram_profile_data_from_apify(username)
       profile_url = "https://www.instagram.com/#{username}/"
       Media.apify_request(profile_url, :instagram)
+    end
+
+    def html_for_instagram_profile(html_page, request_url)
+      return unless html_page
+
+      request_url = request_url + "/" unless request_url.end_with? "/"
+
+      '<div><iframe src="' + request_url + 'embed" width="397" height="477" frameborder="0" scrolling="no" allowtransparency="true"></iframe></div>'
     end
   end
 end
