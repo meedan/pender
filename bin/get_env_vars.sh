1b0af92822d91735dd6f95dadbc3f5882a076418
Doci Mou <22457019+dmou@users.noreply.github.com>
2024-11-11 12:05:10 -0500

    remove .env before recreating

--- bin/get_env_vars.sh
+++ bin/get_env_vars.sh
@@ -6,16 +6,16 @@ if [[ $? != 0 ]]; then
   echo "Error calling AWS get-caller-identity. Do you have valid credentials?"
 else
   SSM_NAMES=$(aws ssm get-parameters-by-path --region $REGION --path /test/pender/ --recursive --with-decryption --output text --query "Parameters[].[Name]")
-  echo "Getting variables"
+
+  if [ -f ".env" ]; then
+    echo "File .env exists, removing."
+    rm .env
+  fi
+  echo "Getting SSM variables"
   for NAME in $SSM_NAMES; do
     echo "."
     VALUE=$(aws ssm get-parameters --region $REGION --with-decryption --name "$NAME" | jq .Parameters[].Value)
     VARNAME=$(basename "$NAME")
-
-    if [[ "$DEPLOY_ENV" == "test" ]]; then
-      echo "${VARNAME}=${VALUE}" >> .env.test
-    else
-      echo "$VARNAME=$VALUE" >> .env
-    fi
+    echo "$VARNAME=$VALUE" >> .env
   done
 fi
