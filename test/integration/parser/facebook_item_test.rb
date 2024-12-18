0f9719e665513707b989f6a0a89126f10bb10692
Jay Joshua <joejoshw@gmail.com>
2024-12-18 14:30:43 +0100

    Remove author url  assertion for apify

--- test/integration/parser/facebook_item_test.rb
+++ test/integration/parser/facebook_item_test.rb
@@ -12,7 +12,6 @@ class FacebookItemIntegrationTest < ActiveSupport::TestCase
     assert !data['title'].blank?
     assert !data['username'].blank?
     assert !data['author_name'].blank?
-    assert !data['author_url'].blank?
     assert !data['description'].blank?
     assert !data['text'].blank?
     assert !data['picture'].blank?
