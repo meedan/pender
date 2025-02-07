7cf545ece2a516ad39c917e5c481ba63465e9128
manu vasconcelos <manu@meedan.com>
2025-02-07 11:15:02 -0300

    Increases the interval between retries
    1 minute turned out to not be enough, I copied the interval from
    archiver_worker

--- app/sidekiq/archiver_status_job.rb
+++ app/sidekiq/archiver_status_job.rb
@@ -2,11 +2,8 @@
 # 1. I used sidekiqs own retrying/backing off formula as a starting point
 #  https://github.com/sidekiq/sidekiq/wiki/Error-Handling#automatic-job-retry
 # 2. start retrying:
-  # aprox. after a minute, that should be more than enough (it usually doesn't take that long)
-# 3. interval:
-  # just add a bit of more time, instead of 15, 60.
-  # It’s a small change but helps us start a bit later, a makes the intervals a bit longer.
-  # I think this is enough to help, as this happens when we are waiting for the request to be processed by archive_org
+  # I think ramping up to 4.5 and starting and always adding 5 minutes might be enough.
+  # It starts later and increases the intervals faster# 3. interval:
 # 4. for how long:
   # 24 hours.
   # If we haven’t been able to get the status after 24 hours should it seems wasteful to keep trying
@@ -16,7 +13,7 @@ class ArchiverStatusJob
   sidekiq_options retry_for: 24.hours, queue: 'archiving'
 
   sidekiq_retry_in do |count|
-    (count ** 4) + 60 + (rand(10) * (count + 1))
+    (count ** 4.5) + 300 + (rand(10) * (count + 1))
   end
 
   def perform(job_id, url, key_id)
