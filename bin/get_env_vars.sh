5a1b3385e1d7e245267487e42ea49227b9a89be8
Doci Mou <22457019+dmou@users.noreply.github.com>
2025-02-21 11:01:33 -0500

    Consolidate dockerfiles for testing (#519)
    * temp remove user cmd
    
    * run tests for this branch
    
    * move workdir
    
    * add a chown
    
    * add debug statements to setup-parallel
    
    * move useradd
    
    * test moving bins to opt/
    
    * more debug msg
    
    * absolute paths
    
    * don't add volumes
    
    * fix docker-entrypoint.sh with DEPLOY_ENV set to test
    
    * print env
    
    * explicitly pass deploy env in ci
    
    * rm env
    
    * back to detached mode
    
    * return to local deploy env
    
    * use tmp dir elsewhere
    
    * delete migration creating default directories
    
    * set default log path to use /var/log
    
    * copy cookies.txt in ci-test-pr workflow
    
    * use new log paths config
    
    * different logpath syntax
    
    * change more paths
    
    * update paths
    
    * attempt to copy schema.rb to /opt/db
    
    * forcibly create /opt/db?
    
    * fix local filepath
    
    * fix mkdir
    
    * fix mkdir
    
    * actually do this in the script
    
    * check schema.rb in
    
    * explicit mkdir /opt/db
    
    * maybe just use /opt?
    
    * mkdir as root??
    
    * add schema.rb
    
    * try new docker test
    
    * comment out path mods
    
    * add chmod on schema.rb
    
    * add debug msg
    
    * chmod a+w
    
    * ok the other file
    
    * only use docker-test.yml
    
    * go back to lograge logger
    
    * use deploy_env override
    
    * log debug mode
    
    * try with rake routes
    
    * fix workflow syntax
    
    * fix workflow syntax
    
    * run docker logs
    
    * run docker logs again
    
    * run docker logs again
    
    * run test no parallel
    
    * add docker inspect
    
    * inspect pender-pender-1
    
    * update syntax
    
    * reset puma dirpath?
    
    * change storage path??
    
    * modify chmod of all files
    
    * try again with parallel tests
    
    * mkdir coverage first
    
    * chmod coverage folder
    
    * add empty coverage dir
    
    * rm otel-collector from docker-test.yml
    
    * sort .gitignore
    
    * remove debug messages from ci test
    
    * run workflow with continue-on-error for tests
    
    * write test ssm to separate file
    
    * use pg_isready and update readme
    
    * create a .env.test in the CI environment
    
    * add build:. as backup for now
    
    * do not use an internal docker network
    
    * add chown flags to copy commands in dockerfile
    
    * try to fix codeclimate ci step
    
    * retry container cp
    
    * docker cp with absolute paths
    
    * change relative path of cc-test-reporter
    
    * run go mod init
    
    * actually cd to go module dir
    
    * run cc-test-reporter without go modules
    
    * check go env
    
    * set proper path of resultset
    
    * put flags at end
    
    * Fixing issues with code coverage report generation and upload to Code Climate (#489)
    
    Fixes the steps that generate and upload the code coverage report to Code Climate.
    
    The main issue was that previously, we ran the tests, prepared the coverage report, and uploaded it all within the Docker container. Now, however, the tests run inside the Docker container, while the coverage report is prepared outside of it, leading to a mismatch in file paths. The solution I implemented was to use sed to replace the paths, changing /app/pender to /home/runner/work/pender. I also needed to copy codeclimate.json to the coverage directory and set the $CC_TEST_REPORTER_ID environment variable.
    
    Reference: CV2-5020.
    
    * remove .env before recreating
    
    * remove references to .env.test
    
    * (1) Using latest Code Climate binary instead of pulling random unexistent branch from GitHub & (2) Fixing error `Error: strconv.Atoi: parsing "2025-02-13T09:46:37-05:00": invalid syntax`.
    
    * address PR comments
    
    ---------
    
    Co-authored-by: Caio Almeida <117518+caiosba@users.noreply.github.com>

--- bin/get_env_vars.sh
+++ bin/get_env_vars.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 export REGION=eu-west-1
 aws sts get-caller-identity >/dev/null 2>&1
-if (( $? != 0 )); then
+
+if [[ $? != 0 ]]; then
   echo "Error calling AWS get-caller-identity. Do you have valid credentials?"
-else 
+else
   SSM_NAMES=$(aws ssm get-parameters-by-path --region $REGION --path /test/pender/ --recursive --with-decryption --output text --query "Parameters[].[Name]")
-  echo "Getting variables"
+
+  if [ -f ".env" ]; then
+    echo "File .env exists, removing and overwriting."
+    rm .env
+  fi
+  echo "Getting SSM variables"
   for NAME in $SSM_NAMES; do
     echo "."
     VALUE=$(aws ssm get-parameters --region $REGION --with-decryption --name "$NAME" | jq .Parameters[].Value)
     VARNAME=$(basename "$NAME")
-
     echo "$VARNAME=$VALUE" >> .env
   done
 fi
