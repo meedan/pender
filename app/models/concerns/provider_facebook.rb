903c7f244c8f5cec748c4c92f11f68ec37fa0c10
manu vasconcelos <manu@meedan.com>
2025-05-02 11:04:55 -0300

    fix regex

--- app/models/concerns/provider_facebook.rb
+++ app/models/concerns/provider_facebook.rb
@@ -11,7 +11,7 @@ module ProviderFacebook
         { pattern: /^https:\/\/([^\.]+\.)?facebook.com\/login/, reason: :login_page },
         { pattern: /^https:\/\/([^\.]+\.)?facebook.com\/?$/, reason: :login_page },
         { pattern: /^https:\/\/([^\.]+\.)?facebook.com\/cookie\/consent-page/, reason: :consent_page },
-        { pattern: /^https:\/\/([^\.]+\.)?facebook.com\/watch/, reason: :watch_page }
+        { pattern: /^https:\/\/([^\.]+\.)?facebook.com\/watch\/*?$/, reason: :watch_page }
       ]
     end
 
