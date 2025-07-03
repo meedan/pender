59ffd289f9d90c4fba58aaaae09c2efe19f2e2ba
manu vasconcelos <manu@meedan.com>
2025-07-03 16:26:14 -0300

    parsed apify response

--- app/models/concerns/media_apify_item.rb
+++ app/models/concerns/media_apify_item.rb
@@ -69,7 +69,7 @@ module MediaApifyItem
     def self.handle_response(response, apify_url)
       Rails.logger.info level: 'INFO', message: '[Parser] Initiated scraping job on Apify', url: apify_url
 
-      parsed_response = JSON.parse(response.body)
+      parsed_response = JSON.parse(response.body) unless response.nil?
       raise ApifyResponseError if response.nil? || !['200', '201'].include?(response.code) || response.body.blank?
       raise ApifyResponseError if response.body.include?("This content isn't available")
 
@@ -88,14 +88,27 @@ module MediaApifyItem
 
     def self.handle_apify_error(error, apify_url, parsed_response)
       apify_url = URI.parse(apify_url).path
+      apify_response_url, apify_response_error, apify_response_error_description = parsed_apify_response(parsed_response)
 
       PenderSentry.notify(
         ApifyError.new(error),
         apify_url: apify_url,
         error_message: error.message,
-        apify_response: parsed_response
+        apify_response_url: apify_response_url,
+        apify_response_error: apify_response_error,
+        apify_response_error_description: apify_response_error_description,
       )
       Rails.logger.warn level: 'WARN', message: '[Parser] Could not process Apify request', apify_url: apify_url, error_class: error.class
+    end
+
+    def self.parsed_apify_response(parsed_response)
+      apify_response = parsed_response.nil? ? {} : parsed_response.first
+
+      apify_response_url = apify_response['url'].presence
+      apify_response_error = apify_response['error'].presence
+      apify_response_error_description = apify_response['errorDescription'].presence
+
+      [apify_response_url, apify_response_error, apify_response_error_description]
     end
   end
 end
