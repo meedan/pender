077a7666df7022bd261cb0c29fde3a97a32a02d4
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2025-08-05 17:46:20 -0300

    6210 – skip_get_canonical_url when the host is Instagram (#572)
    When we try to get the canonical URL for an Instagram media in production
    environment, it always results in a redirection loop. This error happens
    in OpenURI, so there is not a lot we can do to fix it.
    
    The error does not affect the functionality of the application,
    but it causes unnecessary processing and is really noisy (since we get a
    notification in Sentry every time).
    
    Since the parsing of instagram links works fine without the canonical URL,
    we are skipping this step, but logging when we do, if we need to review
    at some point.
    
    References: CV2-6210

--- test/integration/parser/page_item_test.rb
+++ test/integration/parser/page_item_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class PageItemIntegrationTest < ActiveSupport::TestCase
   test "should parse a given site" do
-    m = create_media url: 'https://noticias.uol.com.br/'
+    m = create_media url: 'https://meedan.com/blog'
     data = m.as_json
     assert_equal 'item', data['type']
     assert_equal 'page', data['provider']
-    assert_match /Acompanhe as últimas notícias do Brasil e do mundo/, data['title']
+    assert_not_nil data['title']
     assert_not_nil data['description']
     assert_not_nil data['published_at']
     assert_equal '', data['username']
-    assert_equal 'https://noticias.uol.com.br', data['author_url']
-    assert_equal 'UOLNoticias', data['author_name']
+    assert_equal 'https://meedan.com', data['author_url']
+    assert_not_nil data['author_name']
     assert_not_nil data['picture']
     assert_nil data['error']
   end
