fb960acff127bf474a1e00e9d530454ee9dd7bc2
manu vasconcelos <manu@meedan.com>
2025-08-29 13:36:48 -0300

    Make sure cache is clean before each individual test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,6 +31,7 @@ class ActiveSupport::TestCase
   # Shared setup/teardown for tests as we make
   # the unit tests isolated
   def isolated_setup
+    Rails.cache.clear
     WebMock.enable!
     WebMock.disable_net_connect!(allow: [/minio/])
     Sidekiq::Testing.fake!
@@ -38,6 +39,7 @@ class ActiveSupport::TestCase
   end
 
   def isolated_teardown
+    Rails.cache.clear
     Sidekiq::Worker.clear_all
     Sidekiq::Testing.inline! # reset, to match current test_helper teardown
     WebMock.reset!
@@ -50,6 +52,7 @@ class ActiveSupport::TestCase
   def setup
     # For debugging only: print the test name before it runs
     # puts "#{self.class.name}::#{self.method_name}"
+    Rails.cache.clear
     Sidekiq::Testing.inline!
     Rails.application.reload_routes!
     Media.any_instance.stubs(:archive_to_archive_org).returns(nil)
@@ -63,6 +66,7 @@ class ActiveSupport::TestCase
   # This will run after any test
 
   def teardown
+    Rails.cache.clear
     WebMock.reset!
     WebMock.allow_net_connect!
     Media::ARCHIVERS['perma_cc'][:enabled] = false
